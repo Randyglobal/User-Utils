63b51252f246a4870eb38246dc174a6337ddeaa7
Randyglobal <110671224+Randyglobal@users.noreply.github.com>
2022-12-19 11:55:38 -0800

    js in digits

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 /**
  * Accessing the dom tree
  */
-const operationScreen = document.querySelector('input.operaionScreen');
+const operationScreen = document.querySelector('input.operationScreen');
 const answerScreen = document.querySelector('input.answerScreen');
 answerScreen.readOnly = true;
 
 //Then you call the digits
-const digitalOps = document.querySelector('button.digits');
+const digitalOps = document.querySelectorAll('button.digits');
 
 //operation on digits
 //i tried to call a function here using .forEach forgeting that i did not assign the value as a 
 // function. I can just add the event listener directly.
-digitalOps.addEventListener('click', (event) => {
-  event.preventDefault();
-  operationScreen.value = event.target.innerText;
+digitalOps.forEach(digit => {
+   digit.addEventListener('click', (event) => {
+    event.preventDefault();
+    operationScreen.value += event.target.innerText
+   });
 })
