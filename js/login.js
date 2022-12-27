8d1c9b7b3f092bbda50adfac629acac6c3876444
Randyglobal <randyitoe2255@gmail.com>
2022-12-26 16:27:04 -0800

    creat account form

--- js/login.js
+++ js/login.js
@@ -1,3 +1,16 @@
-submit.onclick = () => {
-    localStorage.setItem('label', userName.value);
-}
+// alert("i'm here for password Input")
+let password = window.document.getElementById('password');
+let button = window.document.getElementById('submit');
+
+button.addEventListener('click', function(){
+    if(password.length == 8){
+
+
+    }
+})
+
+// submit.onclick = () => {
+//     localStorage.setItem('label', userName.value);
+// }
+
+
