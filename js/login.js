324efd9a88f12cb6ff5b30ac37ac79f5f897e1e9
Randyglobal <randyitoe2255@gmail.com>
2023-01-05 06:27:50 -0800

    re-arrangement of work

--- js/login.js
+++ js/login.js
@@ -2,10 +2,14 @@
 // let passwordd = window.document.getElementById('password');
 // let button = window.document.getElementById('submit');
 
-let eye = window.document.getElementById('eyeIcon')
+let eye = window.document.getElementById('eyeIcon');
+let eyeClosed = window.document.getElementById('eyeClosed')
 let password = window.document.getElementById('password');
 
+eye.style.display = 'block';
+eyeClosed.style.display = 'none';
 password.type = 'password';
+
 
 // button.addEventListener('click', function(){
 //     if(password.length == 8){
@@ -22,10 +26,25 @@ submit.onclick = () => {
 // }
 
 eye.addEventListener('click', function(){
-    if(password.type =='password'){
+    if( password.type = 'password' && eyeClosed.style.display == 'none'){
         password.type = 'text';
+        eyeClosed.style.display = 'block'
+        // eyeClosed.style.Bottom = '50px'
     }else{
+        eyeClosed.style.display = 'none';
         password.type = 'password';
+
+    }
+})
+eyeClosed.addEventListener('click', function(){
+    if( password.type = 'password' && eye.style.display == 'block'){
+        password.type = 'password';
+        eyeClosed.style.display = 'none'
+        // eyeClosed.style.Bottom = '50px'
+    }else{
+        eyeClosed.style.display = 'none';
+        password.type = 'password';
+
     }
 })
 
